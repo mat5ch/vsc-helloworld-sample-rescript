4b98a8ba22a228c496f5c506423acf367d1b7302
mat5ch <matthias.schott@gmail.com>
2025-03-03 10:03:17 +0100

    Update bash script to check for missing "out" folder

--- rename_file.sh
+++ rename_file.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#Check if the filename is provided
-if [ -z "$1" ]; then
-echo "Usage: $0 <filename>"
-exit 1
-fi
-
 fullpath="$1"
 filename=$(basename "$fullpath")
 dirpath=$(dirname "$fullpath")
+parentpath=$(dirname "$dirpath")
 
+# Check if the folder exists, create it if not
+if [[ ! -d "$parentpath/out" ]]; then
+    mkdir "$parentpath/out"
+fi
+
+# Rename and move ReScript file to "out" folder
 if [[ "$filename" == "Extension.mjs"* ]]; then
     newfilename="extension.js"
-    parentpath=$(dirname "$dirpath")
     mv "$fullpath" "$parentpath/out/$newfilename"
     echo "Renamed '$filename' to '$newfilename'"
 fi
